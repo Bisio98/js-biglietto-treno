183fb35193ea7a609f9bfc0a61293f2955f8ea65
Bisio98 <alberto.bisetti@hotmail.it>
2021-11-12 13:53:14 +0100

    completed

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const price = 0.21
+let tripLength = prompt('Chilometri del viaggio:') ;
+let age = prompt('Età del passeggero:');
+let priceCalculated;
+
+priceCalculated = price * tripLength;
+
+if((age < 65) && (age > 18 )){
+}
+
+else if(age > 65){
+    priceCalculated *= 0.4;
+}
+
+else {
+    priceCalculated *= 0.2; 
+}
+
+priceCalculated = priceCalculated.toFixed(2);
+
+alert(`Prezzo: ${priceCalculated}€`)
